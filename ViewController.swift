91e7b98d368d1fcd18ed48ef72f05b2ea7070376
Ula Chen <ulachen@UladeMacBook-Air.local>
2018-12-03 21:00:56 +0800

    all

---
+++ ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BMI_calculator
+//
+//  Created by Ula Chen on 2018/12/3.
+//  Copyright © 2018年 Ula Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton){
+        
+    }
+    
+    @IBAction func resetPressed(_ sender: UIButton){
+        
+    }
+
+
+}
+
